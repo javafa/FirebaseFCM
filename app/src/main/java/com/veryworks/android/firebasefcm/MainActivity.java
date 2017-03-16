cf720c0f2ad0c10a05f077aa1ab97c5736c2a458
javafa <nigne2016@gmail.com>
2017-03-16 12:53:13 +0900

    연계서버 추가

--- app/src/main/java/com/veryworks/android/firebasefcm/MainActivity.java
+++ app/src/main/java/com/veryworks/android/firebasefcm/MainActivity.java
@@ -21,6 +21,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -67,9 +72,52 @@ public class MainActivity extends AppCompatActivity {
     public void sendNotification(View view){
 
         String msg = editMsg.getText().toString();
-        if(!"".equals(msg)){ // 입력값이 있으면 노티를 날려준다
-
+        String token = textToken.getText().toString();
+        if("".equals(msg)){ // 입력값이 있으면 노티를 날려준다
+            Toast.makeText(this,"메시지를 입력하세요!",Toast.LENGTH_SHORT).show();
+            return;
+        }else if("".equals(token)){
+            Toast.makeText(this,"받는사람을 선택하세요!",Toast.LENGTH_SHORT).show();
+            return;
         }
+
+        String result = "";
+
+        // 1. 내 서버정보 세팅
+        String server_url = "http://192.168.1.156:8080/";
+        // 2. 서버로 전송할 POST message 세팅
+        String post_data = "to_token="+token+"&msg="+msg;
+
+        try {
+            // 3. HttpUrlConnection 을 사용해서 내 서버로 메시지를 전송한다
+            //     a.서버연결
+            URL url = new URL(server_url);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            //     b.header 설정
+            con.setRequestMethod("POST");
+            //     c.POST데이터(body) 전송
+            con.setDoOutput(true);
+            OutputStream os = con.getOutputStream();
+            os.write(post_data.getBytes());
+            os.flush();
+            os.close();
+            //     d.전송후 결과처리
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // code 200
+                // 결과처리후 내 서버에서 발송된 결과메시지를 꺼낸다.
+                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String dataLine = "";
+                // 메시지를 한줄씩 읽어서 result 변수에 담아두고
+                while ((dataLine = br.readLine()) != null) {
+                    result = result + dataLine;
+                }
+                br.close();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        // 결과처리된 메시지를 화면에 보여준다
+        Toast.makeText(this,result,Toast.LENGTH_SHORT).show();
     }
 
     public void signIn(View view){
